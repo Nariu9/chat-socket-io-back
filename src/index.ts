8f094f809aaaa16ae1b2ace435ec860c221531fb
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-11-22 21:56:40 +0200

    init: install socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import express, {Request, Response} from 'express'
+import http from 'http'
+import {Server, Socket} from 'socket.io'
 
-const app = express()
-const port = 5000
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
-})
+    res.send('Hello, it\'s WS server');
+});
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+io.on('connection', (socket: Socket) => {
+    console.log('a user connected');
+});
+
+server.listen(5000, () => {
+    console.log('listening on *:5000');
+});
