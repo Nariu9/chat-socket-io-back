38ed1a1b46273d0bc1b5ef79b0df7d072f0f1d87
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-12-20 15:28:28 +0200

    feat: add cors origin

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,9 @@ import cors from 'cors'
 const port = process.env.PORT || 5000
 
 const app = express();
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:3000/'
+}))
 const server = http.createServer(app);
 const io = new Server(server);
 
